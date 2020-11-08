bb26e0b96ad5b52c200b158e1bb590d594bccdf1
Johbra96 <61549336+Johbra96@users.noreply.github.com>
2020-11-08 20:08:14 +0100

    sideBar design

--- src/Components/sidebar/Sidebar.jsx
+++ src/Components/sidebar/Sidebar.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
+import logo from '../../images/logo.png'
+
+const sideBarStyle = {
+  backgroundColor: "#815757",
+  height: "250px",
+  width: "500px"
+}
+
+const logoStyle = {
+  width: "150px",
+  margin: "40px 0px 0px 35px"
+}
 
 const Sidebar = (props) => {
   return (
     <Menu>
+      <div style={sideBarStyle}> <img src={logo} style={logoStyle}/> </div>
       <a className="menu-item" href="/">
         Home
       </a>
